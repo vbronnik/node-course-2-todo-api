0f9e76882944d1623e0658985f7bfd1763cf8d35
Veronika Lammon <vbronnik@Veronikas-MacBook-Pro.local>
2016-12-06 19:10:23 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,29 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+// Create fake todo array for testing GET route
+const todos = [{
+  text: "First test todo"
+}, {
+  text: "Second test todo"
+}];
+
 // setting up the DB each time before each run
-// Remove all elements
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  // Remove all elements for POST /todos route testing
+  // Todo.remove({}).then(() => done());
+
+  // Insert items from an array for GET and POST /todos route testing
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos); // return to allow for .then
+  }).then(() => {
+    done();
+  }), (err) => {
+    if (err) {
+      console.log('Unable to insert many', err);
+      done(err);
+    }
+  }
 });
 
 describe('POST /todos', () => {
@@ -26,7 +45,8 @@ describe('POST /todos', () => {
           return done(err); // return error to done
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
+          // added {text} in find to look for just that text for the todo on verification
           expect(todos.length).toBe(1);  // assumption is that there is only one in the DB (clear at top)
           expect(todos[0].text).toBe(text);
           done();
@@ -47,9 +67,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
