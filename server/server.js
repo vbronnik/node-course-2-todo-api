3d141ea8837da166730759554f04bb8f1c337428
Veronika Lammon <vbronnik@Veronikas-MacBook-Pro.local>
2016-12-12 12:04:03 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -37,6 +38,32 @@ app.get('/todos', (req, res) => {
   });
 });
 
+// GET /todos/{id}
+// id var is on the request object
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id))
+  {
+    return res.status(404).send(); //'ID not valid');
+  }
+
+  // to test the GET in Postman
+  // res.send(req.params);
+
+  // gets the todo for particular ID
+  Todo.findById(id).then((todo) => {
+    if (!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((err) => {
+    if (err) {
+      res.status(400).send();  //err); - not sending error since might contain private info
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log(`Started on port 3000`);
